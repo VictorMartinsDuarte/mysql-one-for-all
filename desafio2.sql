5f9a5d8154bbea0ae149adb5b534f864937df87f
Victor Martins <victor.martins.duarte@gmail.com>
2022-04-05 12:05:51 -0300

    Req 02 - Refact

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,7 @@
 SELECT
-  (SELECT
-    COUNT(*)
-      FROM SpotifyClone.Songs) AS 'cancoes',
-  (SELECT
-    COUNT(*)
-      FROM SpotifyClone.Artists) AS 'artistas',
-  (SELECT
-    COUNT(*)
-      FROM SpotifyClone.Albums) AS 'albums';
+  COUNT(DISTINCT musicas.musica_id) AS cancoes,
+  COUNT(DISTINCT artistas.artista_id) AS artistas,
+  COUNT(DISTINCT albuns.album_id) AS albuns
+FROM SpotifyClone.musicas AS musicas
+INNER JOIN SpotifyClone.artistas AS artistas
+INNER JOIN SpotifyClone.albuns AS albuns;
